9b8a7c224394d9d825c3cd3226f11c53bb503414
Robert Gatt <rgatt@tdf.mt>
2022-09-30 07:27:43 +0200

    added css and logic

--- src/App.js
+++ src/App.js
@@ -1,7 +1,50 @@
 import "./App.css";
+import React from "react";
+import pattern from "./images/pattern-divider-desktop.svg"
+import dice from "./images/icon-dice.svg"
 
 function App() {
-  return <div className="App">hi</div>;
+
+  const [advice, setAdvice] = React.useState({
+    id: Number,
+    advice: String
+  })
+
+  React.useEffect(() => {
+    getAdvice()
+  }, [])
+
+  async function getAdvice() {
+
+    fetch("https://api.adviceslip.com/advice")
+      .then(async function (response) {
+        return JSON.parse(await response.text());
+      }).then(function (data) {
+        setAdvice(() => {
+          console.log(data)
+          return { id: data.slip.id, advice: data.slip.advice }
+        })
+      });
+  }
+
+
+  return (
+    <main>
+      <div className="top-div">
+
+        <div className="text-div">
+          <h1>Advice #{advice.id}</h1>
+          <p>{advice.advice}</p>
+          <img className="patternImage" src={pattern} alt="divider"></img>
+        </div>
+
+        <div className="img-div">
+          <img onClick={getAdvice} className="diceImage" src={dice} alt="dice"></img>
+        </div>
+
+      </div>
+    </main>
+  )
 }
 
 export default App;
